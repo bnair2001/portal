1a0b5dda54e991a94b7acf6f1e455cd7554fb39e
Bharath Nair <36124624+bnair2001@users.noreply.github.com>
2018-07-23 22:59:02 +0530

    This one will work I guess

--- src/features/event/eventActions.jsx
+++ src/features/event/eventActions.jsx
@@ -1,20 +1,10 @@
 import { toastr } from 'react-redux-toastr';
 import { FETCH_EVENTS } from './eventConstants';
-import {
-  asyncActionStart,
-  asyncActionFinish,
-  asyncActionError
-} from '../async/asyncActions';
-
+import { asyncActionStart, asyncActionFinish, asyncActionError } from '../async/asyncActions';
+//import { fetchSampleData } from '../../app/data/mockAPI';
 import { createNewEvent } from '../../app/common/util/helpers';
 import moment from 'moment';
 import firebase from '../../app/config/firebase';
-export const fetchEvents = events => {
-  return {
-    type: FETCH_EVENTS,
-    payload: events
-  };
-};
 
 export const createEvent = event => {
   return async (dispatch, getState, { getFirestore }) => {
@@ -40,12 +30,10 @@ export const createEvent = event => {
 export const updateEvent = event => {
   return async (dispatch, getState, { getFirestore }) => {
     const firestore = getFirestore();
-
     if (event.date !== getState().firestore.ordered.events[0].date) {
       event.date = moment(event.date).toDate();
     }
     try {
-      console.log(event);
       await firestore.update(`events/${event.id}`, event);
       toastr.success('Success', 'Event has been updated');
     } catch (error) {
@@ -55,11 +43,7 @@ export const updateEvent = event => {
   };
 };
 
-export const cancelToggle = (cancelled, eventId) => async (
-  dispatch,
-  getState,
-  { getFirestore }
-) => {
+export const cancelToggle = (cancelled, eventId) => async (dispatch, getState, { getFirestore }) => {
   const firestore = getFirestore();
   const message = cancelled
     ? 'Are you sure you want to cancel the event?'
@@ -76,26 +60,70 @@ export const cancelToggle = (cancelled, eventId) => async (
   }
 };
 
-export const getEventsForDashboard = () =>
-  async (dispatch, getState)=>{
-      let today= new Date(Date.now());
-      const firestore= firebase.firestore();
-      const eventsQuery= firestore.collection('events').where('date', '>=', today);
-      try{
-        dispatch(asyncActionStart())
-        let querySnap = await eventsQuery.get()
-        let events=[];
-        for(let i=0;i< querySnap.docs.length;i++)
-        {
-          let evt ={...querySnap.docs[i].data(), id: querySnap.docs[i].id};
-          events.push(evt);
-        }
-        dispatch({type: FETCH_EVENTS, payload: {events}})
-        dispatch(asyncActionFinish())
-      }
-      catch(error){
-          console.log(error);
-          dispatch(asyncActionError())
-      }
+export const getEventsForDashboard = lastEvent => async (dispatch, getState) => {
+  let today = new Date(Date.now());
+  const firestore = firebase.firestore();
+  const eventsRef = firestore.collection('events');
+  try {
+    dispatch(asyncActionStart());
+    let startAfter =
+      lastEvent &&
+      (await firestore
+        .collection('events')
+        .doc(lastEvent.id)
+        .get());
+    let query;
 
+    lastEvent
+      ? (query = eventsRef
+          .where('date', '>=', today)
+          .orderBy('date')
+          .startAfter(startAfter)
+          .limit(2))
+      : (query = eventsRef
+          .where('date', '>=', today)
+          .orderBy('date')
+          .limit(2));
+    
+    let querySnap = await query.get();
+
+    if (querySnap.docs.length === 0) {
+      dispatch(asyncActionFinish());
+      return querySnap;
+    }
+
+    let events = [];
+
+    for (let i = 0; i < querySnap.docs.length; i++) {
+      let evt = { ...querySnap.docs[i].data(), id: querySnap.docs[i].id };
+      events.push(evt);
+    }
+    dispatch({ type: FETCH_EVENTS, payload: { events } });
+    dispatch(asyncActionFinish());
+    return querySnap;
+  } catch (error) {
+    console.log(error);
+    dispatch(asyncActionError());
+  }
+};
+
+export const addEventComment = (eventId, values, parentId) => 
+  async (dispatch, getState, {getFirebase}) => {
+    const firebase = getFirebase();
+    const profile = getState().firebase.profile;
+    const user = firebase.auth().currentUser;
+    let newComment = {
+      parentId: parentId,
+      displayName: profile.displayName,
+      photoURL: profile.photoURL || '/assets/user.png',
+      uid: user.uid,
+      text: values.comment,
+      date: Date.now()
+    }
+    try {
+      await firebase.push(`event_chat/${eventId}`, newComment)
+    } catch (error) {
+      console.log(error);
+      toastr.error('Oops', 'Problem adding comment')
+    }
   }
